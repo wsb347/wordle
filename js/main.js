d8918eeab86134fe3b7187a4e621ef728ca565b3
wsb347 <wsb347@naver.com>
2023-07-05 22:00:32 +0900

    키 눌리는 효과 추가
    //내맘대로 일단 구현
    - 첫줄 5개 입력 완료시 엔터키 입력가능
    - 엔터키 반응 동작 추가
      - 다음줄로 이동
    - 입력시 박스테두리 색 어둡게 만드는 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 // a=65 z=90
+let attempts = 0;
+let index = 0;
+
 function appStart() {
   const handleKeydown = (event) => {
     const keyCode = event.keyCode;
-    const key = event.key;
+    const key = event.key.toUpperCase();
 
     //console.log("키가 눌렸습니다! 누른 값은", keyCode, "입니다");
     //console.log("키가 눌렸습니다! 누른 값은", key, "입니다");
 
-    const thisBlock = document.querySelector(".board-block[data-index='00']");
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+    if (index > 4 && keyCode == 13) {
+      console.log("키가 눌렸습니다! 누른 값은", key, "입니다");
+      index = 0;
+      attempts += 1;
+    } else if (keyCode > 64 && keyCode < 91) {
+      index += 1;
+      console.log(index);
 
-    thisBlock.innerText = key;
+      thisBlock.innerText = key;
+      thisBlock.style.border = " 2px solid rgb(135,138,140)";
+    }
   };
 
   window.addEventListener("keydown", handleKeydown);
